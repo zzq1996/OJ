c950e5acf796fd49929eec7d78e2ed2212aa1a78
zzq1996 <1824837536@qq.com>
2023-06-29 00:56:30 +0800

    Thu Jun 29 12:56:18 AM CST 2023

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zhang/CLionProjects/OJ
+# Install script for directory: /home/zhangzq/CLionProjects/OJ
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/zhang/CLionProjects/OJ/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/zhangzq/CLionProjects/OJ/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
